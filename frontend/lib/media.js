d509c08854428650ca68419c0fa9bcc0a85dd396
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2022-10-16 20:09:39 -0300

    lib

---
+++ frontend/lib/media.js
@@ -0,0 +1,10 @@
+import { getStrapiURL } from './api';
+
+export function getStrapiMedia(media) {
+  if (media.data !== null) {
+    const { url } = media.data.attributes;
+    const imageUrl = url.startsWith('/') ? getStrapiURL(url) : url;
+
+    return imageUrl;
+  }
+}
